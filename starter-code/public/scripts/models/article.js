da27a40fbfaa07bb94f7b2003691a432fb99a241
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-08-31 15:13:51 -0700

    article.js comments

--- starter-code/public/scripts/models/article.js
+++ starter-code/public/scripts/models/article.js
@@ -13,13 +13,17 @@ var app = app || {};
 
   // REVIEW: With ES6 arrow functions, if the function only has one parameter, you don't need parentheses.
   //         This is similar to saying Article.loadAll = function(rows).
-    // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+    // COMMENT:(DONE) What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+
+    // method attached to the Article object, each instance of Article (using the rows result from our client.query) is sorted by date and then added to an array called Article.all. It is called in Article.fetchAll. It calls .sort, .map, and the Article constructor function.
   Article.loadAll = rows => {
     rows.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
     Article.all = rows.map(ele => new Article(ele));
   };
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+
+  // Article.fetchAll triggers an HTTP get request using jQuery to retireive the articles from the databse, and then taking the returned results as a parameter and calling the Article.loadAll method. It is called in the function articleController.loadAll in the file articleController.js.
   Article.fetchAll = callback => {
     $.get('/articles')
     .then(
@@ -55,6 +59,8 @@ var app = app || {};
   };
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+
+  // This function takes each author in allAuthors() after the Article.all array has been filtered by author, the body of their articles have been mapped to establish word count, and all their word counts for all their articles have been added together through the reduce. It is called in adminView.initAdminPage(), and it calls Article.allAuthors() and filter, map, and reduce.
   Article.numWordsByAuthor = () => {
     return Article.allAuthors().map(author => {
       return {
@@ -75,6 +81,9 @@ var app = app || {};
   };
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+
+  // This function deletes the table with the exception of the header row where the labels are listed using the $.ajax method to get the url 'tables' and applying the method delete. It can be called in the postgres shell, and it calls a console.log and a callback.
+
   Article.truncateTable = callback => {
     $.ajax({
       url: '/articles',
